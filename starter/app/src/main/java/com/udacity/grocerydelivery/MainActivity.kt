6576781c5f883acbb14c7f2f4016937907a66cc0
biinui <yu.alvinray@gmail.com>
2021-01-06 05:49:48 +0800

    Inflate main activity. Remove appbarlayout and toolbar from main layout. Enable actionbar by using apptheme with actionbar.

--- starter/app/src/main/java/com/udacity/grocerydelivery/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/grocerydelivery/MainActivity.kt
@@ -2,20 +2,23 @@ package com.udacity.grocerydelivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.udacity.grocerydelivery.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
