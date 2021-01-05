e4a37138f984dc8f19a858e3c2fd4d634e3547ab
biinui <yu.alvinray@gmail.com>
2021-01-06 05:22:11 +0800

    Remove unused imports.

--- starter/app/src/main/java/com/udacity/grocerydelivery/screens/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/grocerydelivery/screens/login/LoginFragment.kt
@@ -1,18 +1,14 @@
 package com.udacity.grocerydelivery.screens.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavAction
-import androidx.navigation.NavDestination
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.udacity.grocerydelivery.R
 import com.udacity.grocerydelivery.databinding.FragmentLoginBinding
-import com.udacity.grocerydelivery.screens.welcome.WelcomeFragmentDirections
 
 class LoginFragment : Fragment() {
 
